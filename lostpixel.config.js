d199156f63758f444880d5274a5637154d0ab758
oHTGo <nguyennhathuy.orit@gmail.com>
2024-02-18 22:08:48 +0700

    Update vrt

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
+/**
+ * @type {import('lost-pixel').CustomProjectConfig}
+ */
 export default {
   storybookShots: {
     storybookUrl: './storybook-static',
   },
+  breakpoints: [1920],
   generateOnly: true,
   failOnDifference: true,
   threshold: 0.1,
