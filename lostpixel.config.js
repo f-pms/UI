8d0f4a1320fce02438dfd11b7cdd87febd725c1a
oHTGo <nguyennhathuy.orit@gmail.com>
2024-02-18 21:45:43 +0700

    Update vrt

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -3,6 +3,6 @@ export default {
   storybookShots: {
     storybookUrl: './storybook-static',
   },
-  lostPixelProjectId: 'clsrku9pbp8xhll0eui4grsc3',
-  apiKey: process.env.LOST_PIXEL_API_KEY,
+  generateOnly: true,
+  failOnDifference: true,
 };
