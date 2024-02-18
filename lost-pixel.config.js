e576b16b548ce060fc18b84f580c4544044bec88
oHTGo <nguyennhathuy.orit@gmail.com>
2024-02-18 21:21:33 +0700

    Add vrt

---
+++ lost-pixel.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+export default {
+  storybookShots: {
+    storybookUrl: './storybook-static',
+  },
+  lostPixelProjectId: 'clsrku9pbp8xhll0eui4grsc3',
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
