b01c6c700d4c5dd6311b7b1e7dae5063b3d42104
LeXuanDai0307 <lxdai0307@gmail.com>
2024-03-14 11:58:01 +0700

    refactor: noti action method item

--- src/pages/AlarmManagement/partials/AlarmNotiForm/PushMessageMethod.tsx
+++ src/pages/AlarmManagement/partials/AlarmNotiForm/PushMessageMethod.tsx
@@ -1,77 +1,18 @@
-import { useFormContext } from 'react-hook-form';
-
 import { AlarmActionType } from '~/types';
 
-import { AlarmFormData } from '~/pages/AlarmManagement/helpers/alarmForm';
-import { useUpdateAction } from '~/pages/AlarmManagement/hooks/useUpdateAction';
+import { MethodOption } from '~/pages/AlarmManagement/partials/AlarmNotiForm/MethodOption';
 
-import {
-  DeleteOutlineOutlinedIcon,
-  SaveAsOutlinedIcon,
-} from '~/components/Icons';
-import {
-  Box,
-  FormControl,
-  IconButton,
-  Stack,
-  Tooltip,
-  Typography,
-} from '~/components/MuiComponents';
-
-export interface IPushMessageMethodProps {
+export interface IToastMethodProps {
   onRemoveAction: (value: AlarmActionType) => void;
 }
 
-export function PushMessageMethod({ onRemoveAction }: IPushMessageMethodProps) {
-  const { getValues } = useFormContext<AlarmFormData>();
-  const { handleCreateAction, handleDeleteAction, disabled } = useUpdateAction({
-    actionType: AlarmActionType.PUSH_MESSAGE,
-    onRemoveAction,
-  });
-  const isUpdate = getValues('isUpdate');
-
+export function PushMessageMethod({ onRemoveAction }: IToastMethodProps) {
   return (
-    <Stack
-      component='li'
-      direction='row'
-      justifyContent='space-between'
-      spacing={4}
-    >
-      <Box>
-        <FormControl sx={{ width: '100%' }} variant='outlined'>
-          <Typography sx={{ fontWeight: 'bold' }} variant='subtitle2'>
-            Cảnh báo tới thiết bị di động thông qua ứng dụng <b>PMS-Mobile</b>
-          </Typography>
-          <Typography variant='body2'>
-            Tất cả mọi người đã cài đặt ứng dụng PMS-Mobile đều nhận được cảnh
-            báo
-          </Typography>
-        </FormControl>
-      </Box>
-
-      <Stack alignItems='center' direction='row' justifyContent='center'>
-        {isUpdate && (
-          <Tooltip title='Lưu'>
-            <IconButton
-              aria-label='update'
-              color='green'
-              disabled={disabled}
-              onClick={() => handleCreateAction()}
-            >
-              <SaveAsOutlinedIcon />
-            </IconButton>
-          </Tooltip>
-        )}
-        <Tooltip title='Xóa'>
-          <IconButton
-            aria-label='delete'
-            color='red'
-            onClick={handleDeleteAction}
-          >
-            <DeleteOutlineOutlinedIcon />
-          </IconButton>
-        </Tooltip>
-      </Stack>
-    </Stack>
+    <MethodOption
+      actionType={AlarmActionType.PUSH_MESSAGE}
+      description='Tất cả mọi người đã cài đặt ứng dụng PMS-Mobile sẽ nhận được cảnh báo'
+      title='Cảnh báo qua thiết bị di động'
+      onRemoveAction={onRemoveAction}
+    />
   );
 }
--- src/pages/AlarmManagement/partials/AlarmNotiForm/ToastMethod.tsx
+++ src/pages/AlarmManagement/partials/AlarmNotiForm/ToastMethod.tsx
@@ -1,72 +1,18 @@
-import { useFormContext } from 'react-hook-form';
-
 import { AlarmActionType } from '~/types';
 
-import { AlarmFormData } from '~/pages/AlarmManagement/helpers/alarmForm';
-import { useUpdateAction } from '~/pages/AlarmManagement/hooks/useUpdateAction';
-
-import {
-  DeleteOutlineOutlinedIcon,
-  SaveAsOutlinedIcon,
-} from '~/components/Icons';
-import {
-  FormControl,
-  IconButton,
-  Stack,
-  Tooltip,
-  Typography,
-} from '~/components/MuiComponents';
+import { MethodOption } from '~/pages/AlarmManagement/partials/AlarmNotiForm/MethodOption';
 
 export interface IToastMethodProps {
   onRemoveAction: (value: AlarmActionType) => void;
 }
 
 export function ToastMethod({ onRemoveAction }: IToastMethodProps) {
-  const { getValues } = useFormContext<AlarmFormData>();
-  const { handleCreateAction, handleDeleteAction, disabled } = useUpdateAction({
-    actionType: AlarmActionType.POPUP,
-    onRemoveAction,
-  });
-  const isUpdate = getValues('isUpdate');
-
   return (
-    <Stack
-      component='li'
-      direction='row'
-      justifyContent='space-between'
-      spacing={4}
-    >
-      <FormControl sx={{ width: '100%' }} variant='outlined'>
-        <Typography sx={{ fontWeight: 'bold' }} variant='subtitle2'>
-          Hiện cảnh báo ở trang <b>&quot;Giám sát&quot;</b>
-        </Typography>
-        <Typography variant='body2'>
-          Tất cả mọi người đều có thể xem cảnh báo này
-        </Typography>
-      </FormControl>
-      <Stack alignItems='center' direction='row' justifyContent='center'>
-        {isUpdate && (
-          <Tooltip title='Lưu'>
-            <IconButton
-              aria-label='update'
-              color='green'
-              disabled={disabled}
-              onClick={() => handleCreateAction()}
-            >
-              <SaveAsOutlinedIcon />
-            </IconButton>
-          </Tooltip>
-        )}
-        <Tooltip title='Xóa'>
-          <IconButton
-            aria-label='delete'
-            color='red'
-            onClick={handleDeleteAction}
-          >
-            <DeleteOutlineOutlinedIcon />
-          </IconButton>
-        </Tooltip>
-      </Stack>
-    </Stack>
+    <MethodOption
+      actionType={AlarmActionType.POPUP}
+      description='Tất cả mọi người đều có thể xem cảnh báo'
+      title='Hiện cảnh báo ở trang "Giám sát"'
+      onRemoveAction={onRemoveAction}
+    />
   );
 }
