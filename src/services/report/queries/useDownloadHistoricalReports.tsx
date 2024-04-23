80aebd97cf84d1b778dab0150de34f1f5ca28cc0
Lê Xuân Đại <59761180+LeXuanDai0307@users.noreply.github.com>
2024-04-23 20:37:10 +0700

    feat(PMS-310): Add name for electrical equipment (#87)

--- src/services/report/queries/useDownloadHistoricalReports.tsx
+++ src/services/report/queries/useDownloadHistoricalReports.tsx
@@ -1,5 +1,5 @@
 import { storage } from '~/utils';
-import { toISOStringWithoutTimeZone } from '~/utils/date';
+import { convertDateRange } from '~/utils/date';
 const baseURL = import.meta.env.VITE_API_URL as string;
 
 import {
@@ -20,6 +20,8 @@ export const downloadHistoricalReports = async (
   params: DownloadHistoricalReportsParams,
 ) => {
   const formattedParams = new URLSearchParams();
+  const { start, end } = convertDateRange(params?.startDate, params?.endDate);
+
   if (params.typeIds) {
     formattedParams.append('typeIds', params.typeIds.join(','));
   }
@@ -27,16 +29,10 @@ export const downloadHistoricalReports = async (
     formattedParams.append('ids', params.ids.join(','));
   }
   if (params.startDate) {
-    formattedParams.append(
-      'startDate',
-      toISOStringWithoutTimeZone(params.startDate),
-    );
+    formattedParams.append('startDate', start);
   }
   if (params.endDate) {
-    formattedParams.append(
-      'endDate',
-      toISOStringWithoutTimeZone(params.endDate),
-    );
+    formattedParams.append('endDate', end);
   }
   if (params.sortBy) {
     formattedParams.append('sortBy', params.sortBy);
